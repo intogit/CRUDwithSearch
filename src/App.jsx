299544d231fd1d2e9d72dcb7d5646c9e8e1dedb0
Abhishek Yadav <abs.yadav12@gmail.com>
2022-05-20 11:46:43 +0530

    added links

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Csm from './components/Csm'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
+// Abhishek Yadav
+
+// github: https://github.com/intogit/spottabl_assignment
+
+// netlify: https://customersuccessmanager.netlify.app/
+
 function App() {
 
   return (
